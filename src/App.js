44ca3e6869892d54ea90a714c11d2825b472da5d
naheem adedokun <naheemadedokun21@gmail.com>
2022-06-17 11:39:28 +0100

    commited the search functions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+
+import obj from './object.json'
+import { objValues, findKey, formatInputValue} from './path'
+
 
 function App() {
+  const [query, setQuery] = useState('')
+  const [pathFound, setPathFound] = useState('')
+
+  const handleQuery = (e) => {
+    setQuery(e.target.value)
+  }
+
+  const findPath = () => {
+    let newValue = formatInputValue(query);
+    let result = findKey(obj, newValue);
+    setPathFound(result)
+  };
+
+
+useEffect(() => {
+  findPath()
+}, [query])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='contain'>
+      <h1 className='title'>Search Engine</h1>
+      <p>Guess My Path Game</p>
+
+      <input type="search"  className='input' onChange={handleQuery}/>
+      <div className='card-list'>
+      {objValues.map((value, index) => {
+        return (
+          <div className='card-container' key={index}>
+          <p className='game space'>Guess my Path? 🤔</p>
+          <small className='space'><i>I am not case sensitive</i></small>
+          My object value is: <span><b>{value}</b></span>
+          
+          </div>
+        )
+      })}
     </div>
+    <div className='card-container path-card'>
+      <h3 className='space'>Answer:{pathFound ? `${pathFound}`: ''}</h3>
+      {pathFound ? <p>You found the path 🤝! Congrats</p> : <p>You haven't found a path</p>}
+    </div>
+    </div>
+
+    
   );
 }
 
